245d269b7ed5737fc66b4ea0715b323c03b8db2f
Taulan Zauzanov <53187427+Zauzanov@users.noreply.github.com>
2025-10-27 16:07:06 +0300

    Create tcp-client.py

---
+++ tcp-client.py
@@ -0,0 +1,20 @@
+import socket
+
+target_host = "192.168.204.129"
+target_port = 80
+
+# create socket object - IPv4/TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client to the server
+client.connect((target_host, target_port))
+
+# send some data in the form of bytes
+client.send(b"GET / HTTP/1.1\r\nHost: 192.168.204.129\r\n\r\n")
+
+# receive some data
+response = client.recv(4096)
+
+
+print(response.decode())
+client.close()                                  # close the socket
